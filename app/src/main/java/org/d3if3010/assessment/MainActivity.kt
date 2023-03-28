eaf7cd139d866ace7393de9f6ae854a36ba883e4
Muhammad Hudaibi <muhhudaibi11@gmail.com>
2023-03-29 02:13:18 +0700

    mulai kali yaa

---
+++ app/src/main/java/org/d3if3010/assessment/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if3010.assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
